5d3546b545012dd38884fe521744647557620187
Peter Ramos <peterdramos02@gmail.com>
2019-02-04 14:49:11 -0500

    add logic to auth reducer

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,5 +1,47 @@
-const initialState = {};
+import { 
+  SET_AUTH_TOKEN, 
+  CLEAR_AUTH, 
+  AUTH_REQUEST, 
+  AUTH_SUCCESS, 
+  AUTH_ERROR 
+} from '../actions/auth'
 
-export const authReducer = (state = initialState, actions) => {
+const initialState = {
+  authToken: null,
+  currentUser: null,
+  loading: false,
+  error: null
+};
+
+export const authReducer = (state = initialState, action) => {
+  if (action.type === SET_AUTH_TOKEN) {
+    return {
+      ...state,
+      authToken: action.authToken
+    }
+  } else if (action.type === CLEAR_AUTH) {
+    return {
+      ...state,
+      authToken: null,
+      currentUser: null
+    }
+  } else if (action.type === AUTH_REQUEST) {
+    return {
+      ...state,
+      loading: true
+    }
+  } else if (action.type === AUTH_SUCCESS) {
+    return {
+      ...state,
+      currentUser: action.currentUser,
+      loading: false
+    }
+  } else if (action.type === AUTH_ERROR) {
+    return {
+      ...state,
+      loading: false,
+      error: action.error
+    }
+  }
   return state;
 }
