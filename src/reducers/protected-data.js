2ced939b93410768ec4ede74eb7582efb8700302
Peter Ramos <peterdramos02@gmail.com>
2019-02-04 16:04:56 -0500

    add protected data actions and reducer and include in store

---
+++ src/reducers/protected-data.js
@@ -0,0 +1,21 @@
+import { FETCH_PROTECTED_DATA_SUCCESS, FETCH_PROTECTED_DATA_ERROR} from  '../actions/protected-data'
+
+const initialState = {
+  data: '',
+  error: null
+};
+
+export const protectedDataReducer = (state = initialState, action) => {
+  if (action.type === FETCH_PROTECTED_DATA_SUCCESS) {
+    return {
+      ...state,
+      data: action.data
+    }
+  } else if (action.type === FETCH_PROTECTED_DATA_ERROR) {
+    return {
+      ...state,
+      error: action.error
+    }
+  }
+  return state;
+}
--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import { authReducer } from './reducers/auth';
+import { protectedDataReducer } from './reducers/protected-data'
 
 export default createStore(
   combineReducers({
     form: formReducer,
-    authReducer
+    authReducer,
+    protectedDataReducer
   })
 );
