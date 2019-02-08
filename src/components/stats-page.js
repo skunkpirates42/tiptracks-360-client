22d6d7ce651fa74ec25e15648d4b1af2258683d1
Peter Ramos <peterdramos02@gmail.com>
2019-02-08 14:05:02 -0500

    fix: calculations for hourly rate and takehome tips

--- src/components/stats-page.js
+++ src/components/stats-page.js
@@ -18,15 +18,21 @@ export class StatsPage extends Component {
     return moment(date).format('dddd, MMMM Do YYYY');
   }
 
+  toNumber (numString) {
+    return Number(numString);
+  }
+
   render() {
     const tips = this.props.tips.map((tip) => {
       const formattedDate = this.genereteFormattedDate(tip.date);
+      const takeHomeTips = tip.totalTips - tip.tippedOut;
+      const hourlyRate = (takeHomeTips / Number(tip.hours)) + Number(tip.baseWage);
       return (
         <li className="tip-report" key={tip.id}>
           <p>{formattedDate}</p>
-          <p name="tips">Total Tips: <span>{(tip.totalTips - tip.tippedOut)}</span></p>
+          <p name="tips">Take Home Tips: <span>{takeHomeTips}</span></p>
           <p name="tips">Total Hours: <span>{tip.hours}</span></p>          
-          <p name="tips">Hourly Rate: <span>${(tip.totalTips / tip.hours).toFixed(2)} / hr</span></p>
+          <p name="tips">Hourly Rate: <span>${hourlyRate} / hr</span></p>
           <p name="notes">Notes: <span>{tip.notes}</span></p>
         </li>
       )
