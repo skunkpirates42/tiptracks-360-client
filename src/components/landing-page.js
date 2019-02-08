ae21e26d4f5071578e432aed34550f851ae2f908
Peter Ramos <peterdramos02@gmail.com>
2019-02-08 16:39:43 -0500

    format landing-page.js

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -6,40 +6,40 @@ import LoginForm from './login-form';
 
 export class LandingPage extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            loggedIn: false
-        }
+    this.state = {
+      loggedIn: false
     }
+  }
 
-    componentDidUpdate(prevProps) {
-        if (!prevProps.loggedIn && this.props.loggedIn) {
-            // When we are logged in, refresh the auth token periodically
-            this.setState({loggedIn: true})
-        } else if (prevProps.loggedIn && !this.props.loggedIn) {
-            // Stop refreshing when we log out
-            this.setState({loggedIn: false})
-        }
+  componentDidUpdate(prevProps) {
+    if (!prevProps.loggedIn && this.props.loggedIn) {
+      // When we are logged in, refresh the auth token periodically
+      this.setState({loggedIn: true})
+    } else if (prevProps.loggedIn && !this.props.loggedIn) {
+      // Stop refreshing when we log out
+      this.setState({loggedIn: false})
     }
-    
-    render ()   {
-        // If we are logged in redirect straight to the user's dashboard
-        if (this.state.loggedIn) {
-            return <Redirect to="/dashboard" />;
-        }
-        return (
-            <div className="home">
-                <h2>Welcome to Tip Tracks 360</h2>
-                <LoginForm />
-            </div>
-        );
+  }
+  
+  render ()   {
+    // If we are logged in redirect straight to the user's dashboard
+    if (this.state.loggedIn) {
+      return <Redirect to="/dashboard" />;
     }
-    }
+    return (
+      <div className="home">
+        <h2>Welcome to Tip Tracks 360</h2>
+        <LoginForm />
+      </div>
+    );
+  }
+  }
 
 const mapStateToProps = state => ({
-    loggedIn: state.auth.currentUser !== null
+  loggedIn: state.auth.currentUser !== null
 });
 
 export default connect(mapStateToProps)(LandingPage);
